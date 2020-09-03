b5b56544031e67fcb9871ff47427581d16417f55
Sanjeev Kumar Saini <sanjeevk@pyramid.com>
2020-09-03 13:07:38 +0530

    initial module

---
+++ device-api/src/test/java/com/mycompany/devicemgmt/DeviceMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.devicemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
